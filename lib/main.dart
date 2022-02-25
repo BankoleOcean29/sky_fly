6282a5da3b4c91b5e8951026899bec3a81b6696f
BankoleOcean29 <bankolescripted@gmail.com>
2022-02-25 01:04:21 +0100

    Signup page being implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const SkyFly());
+  runApp( SkyFly());
 }
 
 class SkyFly extends StatelessWidget {
   const SkyFly({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue[200],
-    child: Column(
-      children: const [
-        Padding(
-          padding: EdgeInsets.only(top: 20.0, left: 30),
-          child: Text('SkyFly'),
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.blue[300],
+          body: Column(
+            children: [
+              Center(
+                  child: Padding(
+                padding: EdgeInsets.only(top: 70.0),
+                child: Text(
+                  'SkyFly',
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.orangeAccent),
+                ),
+              )),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child:
+                      Text('Welcome Onboard!', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: 10,
+                  ),
+                  child: Text('Let\'s get you started'),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20, right: 20, left: 20),
+                  child: TextField(
+                    decoration:
+                        InputDecoration(hintText: 'Full name'),
+                  ),
+                ),
+              ),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 20, right: 20, left: 20),
+                child: Expanded(
+                  child: TextField(
+                    decoration:
+                    InputDecoration(hintText: 'Email address')),
+                ),)
+            ),
+              Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20, right: 20, left: 20),
+                    child:  TextField(
+                        decoration:
+                        InputDecoration(hintText: 'Password')),)),
+              Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20, right: 20, left: 20),
+                    child: TextField(
+                        decoration:
+                        InputDecoration(hintText: 'Confirm your password')),)),
+              ElevatedButton(onPressed: (){
+                print('tes');
+              }, child: Text('Sign Up'), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orangeAccent)) ,),
+            ],
+          ),
+
         ),
-      ],
-    ),);
+      ),
+    );
   }
 }
-
-
